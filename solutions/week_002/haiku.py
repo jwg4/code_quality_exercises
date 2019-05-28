2a2040d859f4536583b6b8a97e45bc4dd1b37ab8
Jack Grahl <jack.grahl@gmail.com>
2019-05-28 22:15:05 +0000

    Change into a function.

--- solutions/week_002/haiku.py
+++ solutions/week_002/haiku.py
@@ -1,2 +1,4 @@
-lambda s:[len(split('\s+',sub('[aeiouy][^aeiouy ]+','X ',t,0,I).strip()))for t in s.split('\n')]==[5,7,5]
-from re import*
+from re import *
+
+def is_haiku(s):
+    return [len(split('\s+',sub('[aeiouy][^aeiouy ]+','X ',t,0,I).strip()))for t in s.split('\n')]==[5,7,5]
