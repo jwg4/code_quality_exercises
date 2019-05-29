e387f2600ed539f27e77bc5d3b5f2ca7cae5eaf6
Jack Grahl <jack.grahl@gmail.com>
2019-05-29 21:17:32 +0000

    more spacing.

--- solutions/week_002/haiku.py
+++ solutions/week_002/haiku.py
@@ -3,6 +3,11 @@ import re
 
 def is_haiku(s):
     return [
-        len(re.split('\s+', re.sub('[aeiouy][^aeiouy ]+', 'X ', t, 0, re.I).strip()))
+        len(
+            re.split(
+                '\s+',
+                re.sub('[aeiouy][^aeiouy ]+', 'X ', t, 0, re.I).strip()
+            )
+        )
         for t in s.split('\n')
     ] == [5, 7, 5]
