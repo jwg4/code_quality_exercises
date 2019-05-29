c304d69a319f467268a4a79a0fff7fad4aa09f1a
Jack Grahl <jack.grahl@gmail.com>
2019-05-29 21:14:06 +0000

    spacing

--- solutions/week_002/haiku.py
+++ solutions/week_002/haiku.py
@@ -1,4 +1,7 @@
 from re import *
 
 def is_haiku(s):
-    return [len(split('\s+',sub('[aeiouy][^aeiouy ]+','X ',t,0,I).strip()))for t in s.split('\n')]==[5,7,5]
+    return [
+        len(split('\s+',sub('[aeiouy][^aeiouy ]+','X ',t,0,I).strip()))
+        for t in s.split('\n')
+    ] == [5, 7, 5]
