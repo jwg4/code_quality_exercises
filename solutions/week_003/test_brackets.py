11d70efdedb60dc1b90c84424cad56917f57e927
Jack Grahl <jack.grahl@gmail.com>
2019-07-13 21:07:11 +0000

    Add some more tests with much longer strings.

--- solutions/week_003/test_brackets.py
+++ solutions/week_003/test_brackets.py
@@ -43,3 +43,42 @@ class TestSomeInvalidStrings(TestCase):
 
     def test_overlapping_different_kinds(self):
         self.assertFalse(validate("({)}"))
+
+
+class TestLongStrings(TestCase):
+    LARGE_NUMBER = 100_000
+
+    @classmethod
+    def setUpClass(cls):
+        cls.open_round = "(" * cls.LARGE_NUMBER
+        cls.close_round = ")" * cls.LARGE_NUMBER
+        cls.open_multi = "{[" * cls.LARGE_NUMBER
+        cls.close_multi = "]}" * cls.LARGE_NUMBER
+
+    def test_many_matched_round_pairs(self):
+        s = self.open_round + "X" + self.close_round
+        self.assertTrue(validate(s))
+
+    def test_many_unmatched_round_pairs(self):
+        s = self.open_round + ")" + self.close_round
+        self.assertFalse(validate(s))
+
+    def test_many_matched_round_pairs_then_stray_open(self):
+        s = "" + self.open_round + self.close_round + "("
+        self.assertFalse(validate(s))
+
+    def test_many_matched_square_and_curly(self):
+        s = "()" + self.open_multi + "()" + self.close_multi + "()"
+        self.assertTrue(validate(s))
+
+    def test_interspersed_round_pair(self):
+        s = "(" + self.open_multi + ")" + self.close_multi + "()"
+        self.assertFalse(validate(s))
+
+    def test_two_interspersed_round_pairs(self):
+        s = "()" + self.open_multi + ")(" + self.close_multi + "()"
+        self.assertFalse(validate(s))
+
+    def test_completely_wrong(self):
+        s = "[[]" + self.open_round + "){{}}" + self.close_multi + "(][][])"
+        self.assertFalse(validate(s))
